43b2e993c1d80e60eb36070589aa04ee59daf2ad
hawk367security <hawksec367@gmail.com>
2024-11-28 01:05:08 -0300

    Update seguranca.sh

--- seguranca.sh
+++ seguranca.sh
@@ -1,12 +1,10 @@
 apt upgrade && apt upgrade -y
 
+#clamav instalation
 sudo apt install clamav
-sudo apt install clamav-daemon
-
-sudo apt install snort 
-
-
-
+clamav-daemon-y
+apt install clamtk -y
+chmod 777 /var/log/clamav/freshclam.log
 
 #wireshark instalation
 sudo apt install wireshark 
@@ -20,5 +18,6 @@ sudo chgrp wireshark /usr/bin/dumpcap
 sudo chmod 750 /usr/bin/dumbcap 
 
 sudo setcap cap_net_raw,cap_net_admin=eip /usr/bin/dumpcap 
-
+#snort instalation
+sudo apt install snort 
 
