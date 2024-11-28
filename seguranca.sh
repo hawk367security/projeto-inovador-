8ef7c992ba1ce22905edaed7fec45e949e68b4a4
hawk367security <hawksec367@gmail.com>
2024-11-27 23:32:42 -0300

    Update seguranca.sh

--- seguranca.sh
+++ seguranca.sh
@@ -2,11 +2,12 @@ apt upgrade && apt upgrade -y
 
 sudo apt install clawav
 
-sudo freshclam
-
 apt install snort 
 
+
+#wireshark instalation
 apt install wireshark 
+sudo freshclam 
 
 sudo groupadd wireshark 
 
@@ -15,4 +16,4 @@ sudo chmod 750 /usr/bin/dumbcap
 
 sudo setcap cap_net_raw,cap_net_admin=eip /usr/bin/dumpcap 
 
-gnome-session-quit --no-prompt
+
